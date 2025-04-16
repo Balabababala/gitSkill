b90e42ce17c7f6a20c219178ddfb48321d2a32fb
Balabababala <139853702+Balabababala@users.noreply.github.com>
2025-04-16 09:56:59 +0800

    0416_2

--- test/src/test/example/testGeneric.java
+++ test/src/test/example/testGeneric.java
@@ -1,10 +1,12 @@
 package test.example;
 
 import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
 
 public class testGeneric {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		employee<String> emp1 =new employee<>("123");
 		System.out.printf("%s%n",emp1.haha);
 		
@@ -13,6 +15,14 @@ public class testGeneric {
 		emp2.haha.add("456");
 		System.out.printf("%s%n",emp2.haha.toString());
 		System.out.printf("你好%s%n",emp2.haha.toString());
+		
+		URL url = new URL("https://example.com");
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
 
 	}	
 }
